e4d0b205b5abc8f3bf72019518215b22ce91475e
Author Name <oscarclarke9900@gmail.com>
2019-01-12 08:34:03 -0400

    implemented simple KNN algoriths and sorted outputs with lodash

--- plinko/score.js
+++ plinko/score.js
@@ -1,13 +1,27 @@
-const _ = require('lodash');
-const outputs = [];
+let outputs = [];
+const predictionPoint = 300;
+const k = 5;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
     outputs.push([dropPosition, bounciness, size, bucketLabel]);
-    console.log(outputs);
-    _.sortBy(outputs, row => row[3])
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+    const result = _.chain(outputs)
+        .map(row => [distance(row[0]), row[3]])
+        .sortBy(row => row[0])
+        .slice(0, k)
+        .countBy(row => row[1])
+        .toPairs()
+        .sortBy(row => row[1])
+        .last()
+        .first()
+        .parseInt()
+    .value()
+  
+    console.log('Your point will probably drop inside bucket #' + result)
 }
 
+function distance(point) {
+	return Math.abs(point - predictionPoint)
+}
